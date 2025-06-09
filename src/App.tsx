0919a2871c94c83e43e463cc5be904ccf6d50a6a
ketace06 <ismail.omaramin06@gmail.com>
2025-06-09 11:31:14 +0200

    feat: design and fake login

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import IntroAnimation from './components/LogoAnimation';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/NavBar';
-import DayNightToggle from './components/DayNightToggle';
 import Content from './components/Content';
-import Intro from './components/Intro';
+import FakeLogin from './components/FakeLoginIntro';
 
 const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     document.title = 'My portfolio';
   }, []);
 
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
-    const storedValue = localStorage.getItem('darkMode');
-    if (storedValue !== null) {
-      return storedValue === 'true';
-    }
-    return window.matchMedia?.('(prefers-color-scheme: dark)').matches ?? false;
-  });
-
-  useEffect(() => {
-    document.body.classList.toggle('dark-mode', isDarkMode);
-    localStorage.setItem('isDarkMode', isDarkMode.toString());
-  }, [isDarkMode]);
-
-  const toggleDarkMode = () => setIsDarkMode((prev) => !prev);
-
   return (
     <>
-      <Navbar />
-      <IntroAnimation />
-      <Intro />
-      <Content />
-      <DayNightToggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      {!isLoggedIn && <FakeLogin onLogin={() => setIsLoggedIn(true)} />}
+      {isLoggedIn && (
+        <div className="app">
+          <Navbar />
+          <Content />
+        </div>
+      )}
     </>
   );
 };
