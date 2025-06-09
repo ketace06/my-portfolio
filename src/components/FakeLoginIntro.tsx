808f7a438268d2260d5a573648b38c3888ecf31c
ketace06 <ismail.omaramin06@gmail.com>
2025-06-09 12:20:41 +0200

    feat: test github pages with an edit

--- src/components/FakeLoginIntro.tsx
+++ src/components/FakeLoginIntro.tsx
@@ -6,7 +6,7 @@ const FakeLogin = ({ onLogin }: { onLogin: () => void }) => {
           src="https://64.media.tumblr.com/d4d58fce4e215a15641c378df2e64b45/e67f7b676443ff36-2c/s1280x1920/a60668dc192ad3273f00efa48b29ced333b3f55c.png"
           className="avatar"
         />
-        <h2 className="username">User</h2>
+        <h2 className="username">User1</h2>
         <button className="login-button" onClick={onLogin}>
           Login
         </button>
